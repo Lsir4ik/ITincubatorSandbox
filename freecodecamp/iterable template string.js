b2ef366d3494d8bdf42af800ac4a248bf2b099ce
Sergey L <lazarev171193@mail.ru>
2023-01-12 20:49:00 +0400

    изучил, как работает map. Была лишняя переменная 'redundant'

--- freecodecamp/iterable template string.js
+++ freecodecamp/iterable template string.js
@@ -4,9 +4,7 @@ const result = {
     skipped: ["no-extra-semi", "no-dup-keys"]
 };
 function makeList(arr) {
-    // Only change code below this line
     return arr.map(item => `<li class="text-warning">${item}</li>`);
-    // Only change code above this line
 }
 
 const failuresList = makeList(result.failure);
