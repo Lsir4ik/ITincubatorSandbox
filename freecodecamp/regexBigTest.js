2db3885a858284097324bd7028301dab3b5b5d86
Sergey L <lazarev171193@mail.ru>
2023-01-15 12:38:31 +0400

    regextest

---
+++ freecodecamp/regexBigTest.js
@@ -0,0 +1,15 @@
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]([0-9]{2,}|[a-z]+\d*)$/i; // Change this line
+let result = userCheck.test(username);
+console.log(result)
+
+/*
+^ - start of input
+[a-z] - first character is a letter
+[0-9]{2,} - ends with two or more numbers
+| - or
+[a-z]+ - has one or more letters next
+\d* - and ends with zero or more numbers
+$ - end of input
+i - ignore case of input
+ */
